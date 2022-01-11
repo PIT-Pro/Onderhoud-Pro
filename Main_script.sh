23f0ee0f6d3ce181757162dae39b42c9832ff066
Thomas Boom <thomas.boom@pitpro.nl>
2022-01-11 14:12:14 +0100

    1

--- Main_script.sh
+++ Main_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
 #
-/Library/Addigy/macmanage/MacManage.app/Contents/MacOS/MacManage action=notify title="PIT Pro Onderhoud" description="PIT Pro onderhoud gaat starten. Je computer kan trager aanvoelen, maar je kunt wel doorwerken." closeLabel="Oké" timeout="59" && proceed=1
+/Library/Addigy/macmanage/MacManage.app/Contents/MacOS/MacManage action=notify title="PIT Pro Onderhoud" description="PIT Pro onderhoud gaat starten. Je computer kan tijdelijk traag aanvoelen." closeLabel="Oké" timeout="59" && proceed=1
 #
 LOGFILE=/Users/Shared/maintenance_log.txt
 
