caafcd869f3d5ee2aa1f180d2cdc7ca6e6915e21
Thomas Boom <thomas.boom@pitpro.nl>
2022-01-11 11:28:12 +0100

    main

--- Main_script.sh
+++ Main_script.sh
@@ -4,13 +4,13 @@ loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConso
 /Library/Addigy/macmanage/MacManage.app/Contents/MacOS/MacManage action=notify title="PIT Pro Onderhoud" description="PIT Pro onderhoud gaat starten. Je computer kan trager aanvoelen, maar je kunt wel doorwerken." closeLabel="Oké" timeout="59" && proceed=1
 #
 echo "Checking MBBR.."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/Resources/MBBR_check.sh)" 1> /dev/null
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/main/Resources/MBBR_check.sh)" 1> /dev/null
 echo "Purging caches.."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/Resources/purge_cache.sh)" 1> /dev/null
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/main/Resources/purge_cache.sh)" 1> /dev/null
 echo "Clearing browser caches.."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/Resources/clear_browser_caches.sh)" 1> /dev/null
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/main/Resources/clear_browser_caches.sh)" 1> /dev/null
 echo "Checking uptime.."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/Resources/uptime.sh)" 1> /dev/null
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/main/Resources/uptime.sh)" 1> /dev/null
 
 pitproCareLastTime="$(/usr/bin/stat -f "%Sm" -t "%Y%m%d" "/Applications/Utilities/Maintenance.app")" #get the last time PITPro Care has run
 currentDate="$(/bin/date +%Y%m%d)" #get the current date
