b3bd118bb12b534c6aa06df4c34aa1d4dd20e61d
Thomas Boom <thomas.boom@pitpro.nl>
2022-01-11 11:50:13 +0100

    Update Main_script.sh

--- Main_script.sh
+++ Main_script.sh
@@ -3,20 +3,28 @@ loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConso
 #
 /Library/Addigy/macmanage/MacManage.app/Contents/MacOS/MacManage action=notify title="PIT Pro Onderhoud" description="PIT Pro onderhoud gaat starten. Je computer kan trager aanvoelen, maar je kunt wel doorwerken." closeLabel="Oké" timeout="59" && proceed=1
 #
-echo "Checking MBBR.."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/main/Resources/MBBR_check.sh)" 1> /dev/null
-echo "Purging caches.."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/main/Resources/purge_cache.sh)" 1> /dev/null
-echo "Clearing browser caches.."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/main/Resources/clear_browser_caches.sh)" 1> /dev/null
-echo "Checking uptime.."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/main/Resources/uptime.sh)" 1> /dev/null
+
+if [ -e /$loggedinUser/Public/maintenance_log.txt ]
+then
+    echo "maintenance_log.txt exists, writing output to file.."
+else
+    touch $loggedInUser/Public/maintenance_log.txt
+fi
+
+echo "Checking MBBR.." | tee $loggedInUser/Public/maintenance_log.txt
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/main/Resources/MBBR_check.sh)" 1> /$loggedinUser/Public/maintenance_log.txt
+echo "Purging caches.." | tee $loggedInUser/Public/maintenance_log.txt
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/main/Resources/purge_cache.sh)" 1> /$loggedinUser/Public/maintenance_log.txt
+echo "Clearing browser caches.." | tee $loggedInUser/Public/maintenance_log.txt
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/main/Resources/clear_browser_caches.sh)" 1> /$loggedinUser/Public/maintenance_log.txt
+echo "Checking uptime.." | tee $loggedInUser/Public/maintenance_log.txt
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/main/Resources/uptime.sh)" 1> /$loggedinUser/Public/maintenance_log.txt
 
 pitproCareLastTime="$(/usr/bin/stat -f "%Sm" -t "%Y%m%d" "/Applications/Utilities/Maintenance.app")" #get the last time PITPro Care has run
 currentDate="$(/bin/date +%Y%m%d)" #get the current date
 
-echo $pitproCareLastTime > ~/$loggedInUser/Public/maintenance_log.txt
-echo $currentDate > ~/$loggedInUser/Public/maintenance_log.txt
+echo $pitproCareLastTime > $loggedInUser/Public/maintenance_log.txt
+echo $currentDate > $loggedInUser/Public/maintenance_log.txt
 
 /Library/Addigy/macmanage/MacManage.app/Contents/MacOS/MacManage action=notify title="PIT Pro Onderhoud" action=notify description="PIT Pro onderhoud is klaar!" closeLabel="Oké"
 
