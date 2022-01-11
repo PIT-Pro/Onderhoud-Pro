ed8850a21ac39bfb7df3e4e77e5ec36e4b7c30f6
Thomas Boom <thomas.boom@pitpro.nl>
2022-01-11 14:05:12 +0100

    uptime

--- Resources/uptime.sh
+++ Resources/uptime.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 #
-# Uptime to check
-limit="7"
-# Get uptime.
-days=(`uptime | awk '{ print $4 }' | sed 's/,//g'`)
-if [ "$days" -gt "$limit" ]; then
-    echo "Machine needs reboot" 
-    /Library/Addigy/macmanage/MacManage.app/Contents/MacOS/MacManage action=notify title="Herstart aanbevolen" description="Je Mac is al $days dagen niet opnieuw opgestart. Herstarten is aanbevolen." acceptLabel="Herstart" closeLabel="Niet nu" timeout="60" && sudo -u $username osascript -e 'tell app "loginwindow" to Â«event aevtrrstÂ»' || echo "Restart not accepted by user."
-else
-    echo "Machine does not need to reboot, uptime less than $limit days"
+
+/Library/Addigy/macmanage/MacManage.app/Contents/MacOS/MacManage action=notify title="Herstart aanbevolen" description="Herstarten is aanbevolen." acceptLabel="Herstart" closeLabel="Niet nu" timeout="60" && sudo -u $username osascript -e 'tell app "loginwindow" to Â«event aevtrrstÂ»' || echo "Restart not accepted by user."
 fi
 # Uptime check finished
