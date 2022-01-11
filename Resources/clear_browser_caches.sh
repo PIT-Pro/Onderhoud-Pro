3ba1787f15a44e79ce225747b3501ab36e314ef0
Thomas Boom <thomas.boom@pitpro.nl>
2022-01-11 12:32:47 +0100

    uptime

--- Resources/clear_browser_caches.sh
+++ Resources/clear_browser_caches.sh
@@ -2,17 +2,11 @@
 
 #zoek ingelogde user
 loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
-
 #verwijder safari cache
-
 rm -R /Users/$loggedInUser/Library/Caches/com.apple.Safari/WebKitCache/
-
 #verwijder chrome cache
-
 rm -R ~/Library/Caches/Google/Chrome/Default/Cache
 rm -R ~/Library/Caches/Google/Chrome/Default/Code\ Cache
-
 #verwijder edge cache
-
 rm -R /Users/$loggedInUser/Library/Caches/Microsoft\ Edge/Default/Cache/
 rm -R /Users/$loggedInUser/Library/Caches/Microsoft\ Edge/Default/Code\ Cache/
--- Resources/uptime.sh
+++ Resources/uptime.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
+#
 # Uptime to check
 limit="7"
 # Get uptime.
-days=$(uptime | grep -ohe 'up .*' | sed 's/,//g' | awk '{ print $2" " }'))
-# Get logged in user.
-username=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
+days=$(`uptime | awk '{ print $4 }' | sed 's/,//g'`)
 if [ "$days" -gt "$limit" ]; then
     echo "Machine needs reboot" 
     /Library/Addigy/macmanage/MacManage.app/Contents/MacOS/MacManage action=notify title="Herstart aanbevolen" description="Je Mac is al $days dagen niet opnieuw opgestart. Herstarten is aanbevolen." acceptLabel="Herstart" closeLabel="Niet nu" timeout="60" && sudo -u $username osascript -e 'tell app "loginwindow" to Â«event aevtrrstÂ»' || echo "Restart not accepted by user."
