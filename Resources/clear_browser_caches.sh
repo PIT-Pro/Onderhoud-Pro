8431d697bf6eaca1d6fedc0835b1dd8f470a0723
Thomas Boom <thomas.boom@pitpro.nl>
2022-03-11 13:51:10 +0100

    removed python for bash function

--- Resources/clear_browser_caches.sh
+++ Resources/clear_browser_caches.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 #zoek ingelogde user
-loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
-#verwijder safari cache
+loggedInUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }' )#verwijder safari cache
 rm -R /Users/$loggedInUser/Library/Caches/com.apple.Safari/WebKitCache/
 #verwijder chrome cache
 rm -R ~/Library/Caches/Google/Chrome/Default/Cache
--- main.sh
+++ main.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-loggedInUser=`python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");'`
-#
+loggedInUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }' )
 /Library/Addigy/macmanage/MacManage.app/Contents/MacOS/MacManage action=notify title="PIT Pro Onderhoud" description="PIT Pro onderhoud gaat starten. Je computer kan tijdelijk traag aanvoelen." closeLabel="Oké"  && proceed=1
-#
+
 LOGFILE=/Users/Shared/maintenance_log.txt
 
 if [ -e $LOGFILE ]
