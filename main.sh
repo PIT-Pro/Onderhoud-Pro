daa5ec8ca8992308f65a4d60480bebdf82595d51
Thomas Boom <93121029+thomas-boom@users.noreply.github.com>
2022-11-21 16:06:37 +0100

    added log

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 loggedInUser=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ && ! /loginwindow/ { print $3 }')
 
-/Library/Addigy/macmanage/MacManage.app/Contents/MacOS/MacManage action=notify title="PIT Pro Maintenance" description="PIT Pro onderhoud gaat starten. Je computer kan tijdelijk traag aanvoelen." closeLabel="Oké"  && proceed=1
+/Library/Addigy/macmanage/MacManage.app/Contents/MacOS/MacManage action=notify title="PIT Pro Maintenance" description="Click on the button below to start." closeLabel="Run Maintenance"  && proceed=1
 
 LOGFILE=/Library/Addigy/PIT\ Pro/maintenance_log.txt
 currentDate=$(date +%Y/%m/%d\ %H:%M:%S)
@@ -23,11 +23,11 @@ echo "Checking MBBR.." >> "$LOGFILE"
 echo "Purging caches.." >> "$LOGFILE"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/main/Files/purge_cache.sh)"
 echo "Clearing browser caches.." >> "$LOGFILE"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/main/Files/clear_browser_caches.sh)" | tee -a "$LOGFILE"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/main/Files/clear_browser_caches.sh)" >> "$LOGFILE"
 echo "Kickstarting softwareupdated" >> "$LOGFILE"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/main/Files/softwareupdated.sh)" | tee -a "$LOGFILE"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/PIT-Pro/Maintenance/main/Files/softwareupdated.sh)" >> "$LOGFILE"
 
-/Library/Addigy/macmanage/MacManage.app/Contents/MacOS/MacManage action=notify title="Herstart aanbevolen" description="Herstarten is aanbevolen. Sla belangrijke data op voordat je op Herstart klikt." acceptLabel="Herstart" closeLabel="Niet nu" && sudo -u "$loggedInUser" osascript -e 'tell app "loginwindow" to Â«event aevtrrstÂ»' || echo "Restart not accepted by user." | tee -a "$LOGFILE"
+/Library/Addigy/macmanage/MacManage.app/Contents/MacOS/MacManage action=notify title="Herstart aanbevolen" description="Restarting your Mac is highly recommended. Save your work before clicking on Restart." acceptLabel="Restart" closeLabel="Not now" && sudo -u "$loggedInUser" osascript -e 'tell app "loginwindow" to Â«event aevtrrstÂ»' || echo "Restart not accepted by user." >> "$LOGFILE"
 echo "END OF SCRIPT" >> "$LOGFILE"
 
 exit 0
