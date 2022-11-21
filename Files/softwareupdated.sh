9938768e27b5c7d293bb9ef8a3105e81fd9c8b4e
Thomas Boom <93121029+thomas-boom@users.noreply.github.com>
2022-11-21 16:11:34 +0100

    added log

--- Files/softwareupdated.sh
+++ Files/softwareupdated.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+LOGFILE=/Library/Addigy/PIT\ Pro/maintenance_log.txt
+
 # Kill any Apple Silicon downloads
 if pgrep BrainService; then
     pkill BrainService
@@ -9,11 +11,11 @@ fi
 rm -f /Library/Preferences/com.apple.SoftwareUpdate.plist
 
 # Kill and restart Software Update
-while launchctl kill 9 system/com.apple.softwareupdated; do
+while launchctl kill 9 system/com.apple.softwareupdated; do  >> "$LOGFILE"
     sleep 1
 done
 
-launchctl kickstart system/com.apple.softwareupdated
+launchctl kickstart system/com.apple.softwareupdated  >> "$LOGFILE"
     sleep 1
 mv /Library/Receipts/InstallHistory.plist /Library/Receipts/InstallHistory.plist.old
     sleep 3
