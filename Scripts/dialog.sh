495737d28db0249dcd1f0b16e9be13ee84f51775
Thomas Boom <thomas.boom@pitpro.nl>
2023-03-24 15:35:57 +0100

    Update dialog.sh

--- Scripts/dialog.sh
+++ Scripts/dialog.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 #Run Dialog information for user
-dialog --title "Onderhoud Pro" --titlefont "size=20" --icon "https://raw.githubusercontent.com/PIT-Pro/Onderhoud-Pro/main/Files/logo.png" --overlayicon caution --ontop --small --infobox "PIT Pro B.V. \n - Je kunt ons bereiken via: support@pitpro.nl \n - Of voor spoed: 020-2611450" --messagefont "size=14" --message "Deze app verhelpt de volgende situaties: \n - Problemen met synchronisatie van Cloud opslag (Google Drive, Dropbox) \n\n - Problemen met je internet browser (Safari, Chrome, Edge) \n - Problemen met het updaten/upgrade van je Mac \n\n Let op: Je Mac wordt na dit proces herstart. Sla nu je documenten op.  "
+swiftDialog(){
+    dialog --title "Onderhoud Pro" --titlefont "size=20" --icon "https://raw.githubusercontent.com/PIT-Pro/Onderhoud-Pro/main/Files/logo.png" --overlayicon caution --ontop --small --infobox "PIT Pro B.V. \n - Je kunt ons bereiken via: support@pitpro.nl \n - Of voor spoed: 020-2611450" --messagefont "size=14" --message "Deze app verhelpt de volgende situaties: \n - Problemen met synchronisatie van Cloud opslag (Google Drive, Dropbox) \n\n - Problemen met je internet browser (Safari, Chrome, Edge) \n - Problemen met het updaten/upgrade van je Mac \n\n Let op: Je Mac wordt na dit proces herstart. Sla nu je documenten op.  "
+}
+
+swiftDialog
