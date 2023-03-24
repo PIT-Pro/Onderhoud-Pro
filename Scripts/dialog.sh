43a7683c3f8bade88bfc511c005800999ef86fc4
Thomas Boom <thomas.boom@pitpro.nl>
2023-03-24 13:56:35 +0100

    added .json

---
+++ Scripts/dialog.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+dialog --jsonfile "
+
+
+
+
+"https://raw.githubusercontent.com/PIT-Pro/Onderhoud-Pro/main/Files/logo.png" --overlayicon warning --title "Onderhoud Pro" --message "Click on the button below to start." --infobox "### Wat doet Onderhoud Pro?\n  \n - Verwijderd tijdelijke bestanden  \n - Leegt de cache van je internet browser \n - Herstart het Software Update proces"
